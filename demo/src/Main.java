aa6779b654a01b91418f44dc13229f44d71fb219
Alb3rtoGitHub <ravallecillo@hotmail.com>
2023-11-17 21:32:30 -0300

    Saludo a Virginia

--- demo/src/Main.java
+++ demo/src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Probando");
+        System.out.println("Hola Virgina");
     }
 }

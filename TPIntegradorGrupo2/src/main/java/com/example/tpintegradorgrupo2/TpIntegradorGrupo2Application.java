0840350e80331fa8b69dc68ba9383576cafaacfa
Alb3rtoGitHub <ravallecillo@hotmail.com>
2023-11-18 21:15:30 -0300

    Creacion de Proyecto TPIntegradorGrupo2
    creacion con springboot y maven estructura base

---
+++ TPIntegradorGrupo2/src/main/java/com/example/tpintegradorgrupo2/TpIntegradorGrupo2Application.java
@@ -0,0 +1,13 @@
+package com.example.tpintegradorgrupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegradorGrupo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpIntegradorGrupo2Application.class, args);
+    }
+
+}
